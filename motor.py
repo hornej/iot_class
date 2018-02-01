12741c2b08e52a882dfb113dc6ef7632a4d31e07
Josh Horne <joshuastevenhorne@gmail.com>
2018-02-01 11:28:51 -0700

    Update motor.py

--- motor.py
+++ motor.py
@@ -21,26 +21,26 @@ try:
    ##raw_input("Press any key and enter to exit\n")
    
    while True:
+      wp.digitalWrite(Standby, 1)
+      wp.digitalWrite(AIN1, 0)
+      wp.digitalWrite(AIN2, 1)
       for x in range(0, 1000):
          wp.pwmWrite(PWMPin, x) #pwmWrite() takes input values in the range 0 - 1023
-         wp.digitalWrite(Standby, 1)
-         wp.digitalWrite(AIN1, 0)
-         wp.digitalWrite(AIN2, 1)
          time.sleep(.1)
          print(x)
          
       wp.digitalWrite(Standby, 0)
-      time.sleep(1)
+      time.sleep(5)
 
+      wp.digitalWrite(Standby, 1)
+      wp.digitalWrite(AIN1, 1)
+      wp.digitalWrite(AIN2, 0)
       for x in range(0, 1000):
          wp.pwmWrite(PWMPin, x) #pwmWrite() takes input values in the range 0 - 1023
-         wp.digitalWrite(Standby, 1)
-         wp.digitalWrite(AIN1, 1)
-         wp.digitalWrite(AIN2, 0)
          time.sleep(.1)
          
       wp.digitalWrite(Standby, 0)
-      time.sleep(1)
+      time.sleep(5)
       
       
    
