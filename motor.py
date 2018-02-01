bd5efb22d0e5ca9cf3533c710de0cb2235f1fc08
Josh Horne <joshuastevenhorne@gmail.com>
2018-02-01 11:16:16 -0700

    Update motor.py

--- motor.py
+++ motor.py
@@ -5,26 +5,26 @@ import wiringpi as wp
 wp.wiringPiSetupGpio()
 
 #configure input and output pins
-PWMPin = 12
-DirectionPin = 11
-PushBtn1 = 13
-PushBtn2 = 15
-PushBtn3 = 16
+PWMPin = 18
+Standy = 4
+AIN1 = 17
+AIN2 = 24
 
-wp.pinMode(PWMPin, 2)
-wp.pinMode(DirectionPin, 1)
-wp.pinMode(PushBtn1, 0)
-wp.pinMode(PushBtn2, 0)
-wp.pinMode(PushBtn3, 0)
+
+wp.pinMode(PWMPin, 1)
+wp.pinMode(Standy, 1)
+wp.pinMode(AIN1, 1)
+wp.pinMode(AIN2, 1)
 
 try:
    wp.pwmWrite(PWMPin, 250) #pwmWrite() takes input values in the range 0 - 1023
    raw_input("Press any key and enter to exit\n")
+   
 except:
    pass
 finally:
    #clean up
-   print "\nexiting"
+   print("\nexiting")
    wp.pinMode(PWMPin, 0)
    wp.pinMode(DirectionPin, 0)
    exit(0)
